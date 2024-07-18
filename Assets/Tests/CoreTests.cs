11012c05563b5c7b59c2f56129e56e3b4188aa49
Robin Jaspers <Robin.Jaspers@HTW-Berlin.de>
2024-07-18 17:30:20 +0200

    chore: resolved conflict with renaming

--- Assets/Tests/CoreTests.cs
+++ Assets/Tests/CoreTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
-using SaveLoadCore;
 using SaveLoadSystem.Core;
 using SaveLoadSystem.Core.Attributes;
 using SaveLoadSystem.Utility;
